3237c6060fa5964060aee154edac95987b3b707b
Eric Klinkhammer <klinkhae@10-249-91-189.wireless.oregonstate.edu>
2016-10-15 16:53:06 -0700

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -511,11 +511,11 @@ module.exports = function (grunt) {
     'ngAnnotate',
     'copy:dist',
     'cdnify',
-    'cssmin',
-    'uglify',
+   // 'cssmin',
+   // 'uglify',
     'filerev',
-    'usemin',
-    'htmlmin'
+   // 'usemin',
+   // 'htmlmin'
   ]);
 
   grunt.registerTask('dev', [
